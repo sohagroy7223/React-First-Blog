a2b4439072e3f2a14e8863861b5a92575703a715
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-06 10:49:15 +0600

    addad navbar in this page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Navbar from "./assets/components/NavBar/Navbar";
 function App() {
   return (
     <>
-      <h3>Hello world</h3>
+      <Navbar></Navbar>
     </>
   );
 }
